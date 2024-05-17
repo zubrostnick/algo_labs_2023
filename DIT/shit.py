69d48ac48babda11045c7f49b869c27e304d710e
zubrostnick <120424149+zubrostnick@users.noreply.github.com>
2024-05-17 20:57:39 +0200

    Delete DIT/shit.py

--- DIT/shit.py
+++
@@ -1,9 +0,0 @@
-left = 0
-right = len(array)
-while left < right:
-    m = left + (right - left) // 2
-    if x > array[m]:
-        left = m + 1
-    else:
-        right = m
-return array[right] == x
